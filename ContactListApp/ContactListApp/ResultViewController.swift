6b1c85c4ed5f472f981fdc4cb71a6108063f69af
Rohith Sai Ganguru <89401210+Rohith-S545442@users.noreply.github.com>
2022-04-21 14:59:08 -0500

    Firebase Database with TableView

---
+++ ContactListApp/ContactListApp/ResultViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultViewController.swift
+//  ContactListApp
+//
+//  Created by Ganguru,Rohith Sai on 4/21/22.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var phonenumOutlet: UILabel!
+    
+    @IBOutlet weak var emailOutlet: UILabel!
+    
+    var phoneNum = 0
+    var email = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        phonenumOutlet.text = "\(phoneNum)"
+        emailOutlet.text = email
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
